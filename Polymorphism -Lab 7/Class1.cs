6e22f10e89ae30f2f3ab40698182983d715aef26
Daniel Svensson <daniel.svensson682@gmail.com>
2022-10-10 09:23:43 +0200

    knocked out this lab in about 1 hour. nothing new for me, and this is waay easier than last lab...

--- Polymorphism -Lab 7/Class1.cs
+++ Polymorphism -Lab 7/Class1.cs
@@ -9,14 +9,15 @@ namespace Polymorphism__Lab_7
 {
     internal abstract class Sketch
     {
-        private double ImNotAValue = 0;
+        protected double FullArea = 0;
 
-        public virtual double Area(double inValue)
+        public virtual double Area()
         {
 
             Console.WriteLine("This is the Abstract method, Wrong call of the method! Result = 0");
-            return this.ImNotAValue;
+            return this.FullArea;
         }
+
     }
 
     internal class Circle : Sketch
@@ -24,18 +25,52 @@ namespace Polymorphism__Lab_7
         protected float Pi = 3.14159265359f; // just grabbed pi from a calculator..
         protected double Radius { get; set; }
 
-        public override double Area(double radius)
+        public Circle(double radius)
         {
-            Radius = radius * radius * Pi;
-            return this.Radius;
+            this.Radius = radius;
+        }
+
+        public override double Area()
+        {
+            this.FullArea = this.Radius * this.Radius * this.Pi;
+            return this.FullArea;
         }
     }
     internal class Rectangle : Sketch
     {
+        protected double Height { get; set; }
+        protected double Width { get; set; }
 
+        public Rectangle(double height,double width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+        public override double Area()
+        {
+
+            this.FullArea = Height * Width;
+            return this.FullArea;
+        }
     }
     internal class Triangle : Sketch
     {
-
+        protected double TriBase { get; set; }
+        protected double TriHeight { get; set; }
+        protected double TriHypo { get; set; }
+        
+        public Triangle(double triBase, double triHeight, double triHypo)
+        {
+            this.TriBase = triBase;
+            this.TriHeight = triHeight;
+            this.TriHypo = triHypo;
+        }
+        public override double Area()
+        {
+            double SemiAreaOfTriangle = (this.TriBase + this.TriHeight + this.TriHypo) / 2; // calculate the Semi-perimeter
+            double AreaOfTriangle = SemiAreaOfTriangle * (SemiAreaOfTriangle - this.TriBase)
+                * (SemiAreaOfTriangle - this.TriHeight) * (SemiAreaOfTriangle - this.TriHypo); // calculate the full area
+            return Math.Sqrt(AreaOfTriangle);
+        }
     }
 }
--- Polymorphism -Lab 7/Program.cs
+++ Polymorphism -Lab 7/Program.cs
@@ -5,7 +5,12 @@ namespace Polymorphism__Lab_7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Sketch sketch = new Circle(5);
+            Console.WriteLine($"Area of Circle: {sketch.Area()}");
+            sketch = new Rectangle(5, 5);
+            Console.WriteLine($"Area of Rectangle: {sketch.Area()}");
+            sketch = new Triangle(3, 5, 5);
+            Console.WriteLine($"Area of Triangle: {sketch.Area()}");
         }
     }
 }
