88b385c0689741ccc9e6be257512be25cf3bc26a
Daniel Svensson <daniel.svensson682@gmail.com>
2022-10-10 08:59:54 +0200

    Add project files.

---
+++ Polymorphism -Lab 7/Class1.cs
@@ -0,0 +1,41 @@
+﻿//Daniel Svensson .Net22
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism__Lab_7
+{
+    internal abstract class Sketch
+    {
+        private double ImNotAValue = 0;
+
+        public virtual double Area(double inValue)
+        {
+
+            Console.WriteLine("This is the Abstract method, Wrong call of the method! Result = 0");
+            return this.ImNotAValue;
+        }
+    }
+
+    internal class Circle : Sketch
+    {
+        protected float Pi = 3.14159265359f; // just grabbed pi from a calculator..
+        protected double Radius { get; set; }
+
+        public override double Area(double radius)
+        {
+            Radius = radius * radius * Pi;
+            return this.Radius;
+        }
+    }
+    internal class Rectangle : Sketch
+    {
+
+    }
+    internal class Triangle : Sketch
+    {
+
+    }
+}
---
+++ Polymorphism -Lab 7/Program.cs
@@ -0,0 +1,11 @@
+﻿//Daniel Svensson .Net22
+namespace Polymorphism__Lab_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
